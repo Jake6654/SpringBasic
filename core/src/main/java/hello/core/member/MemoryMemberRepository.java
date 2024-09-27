1677bc17ead8ac0d9ed7223efaaa3b936d107b56
jake <ugw2ad@virginia.ede>
2024-09-27 00:57:42 -0400

    add RateDiscountPolicy

--- core/src/main/java/hello/core/member/MemoryMemberRepository.java
+++ core/src/main/java/hello/core/member/MemoryMemberRepository.java
@@ -8,13 +8,13 @@ public class MemoryMemberRepository implements MemberRepository {
     private static Map<Long, Member> store = new HashMap<>(); // Id 와 회원 정보를 Hashmap 에 저장
 
     @Override
-    public void save(Member member) {
+    public void save(Member member) { // 멤버 정보를 입려 받아 저장
         store.put(member.getId(),member);
 
     }
 
     @Override
-    public Member findById(Long memberId) {
+    public Member findById(Long memberId) { // 아이디로 멤버 조회
         return store.get(memberId); // key(Id) 조회 후 value(member) 리턴
     }
 }
