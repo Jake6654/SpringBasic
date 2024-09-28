1f1a6fa5945f3e438f9ca9a7a1ef5244d2fecc98
Jake <ugw2ad@virginia.edu>
2024-09-28 18:41:56 -0400

    Discount policy

--- core/src/main/java/hello/core/AppConfig.java
+++ core/src/main/java/hello/core/AppConfig.java
@@ -1,11 +1,9 @@
 package hello.core;
 
+import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
 import hello.core.discount.RateDiscountPolicy;
-import hello.core.member.Member;
-import hello.core.member.MemberService;
-import hello.core.member.MemberServiceImpl;
-import hello.core.member.MemoryMemberRepository;
+import hello.core.member.*;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
 
@@ -14,12 +12,22 @@ public class AppConfig { // 애플리케이션의 실제 동작에 필요한 구
 
 
     public MemberService memberService(){
-        return new MemberServiceImpl(new MemoryMemberRepository()); // 구체적인 개념을 전달
+        return new MemberServiceImpl(memberRepository());
     }
-    // 생성자 주입
+
     public OrderService orderService(){
-        return new OrderServiceImpl(new MemoryMemberRepository(),new RateDiscountPolicy());
+        return new OrderServiceImpl(memberRepository(),discountPolicy());
     }
+
+    public DiscountPolicy discountPolicy(){
+        return new RateDiscountPolicy();
+        //return new FixDiscountPolicy();
+    }
+
+    private MemberRepository memberRepository(){ // 역할을 더욱 명확하게 구분하여 코드의 가독성을 높임
+        return new MemoryMemberRepository();
+    }
+
 
 
 }
