d4a6efd5b739911653d9894e30961801ac809085
Jake <ugw2ad@virginia.edu>
2024-10-19 10:24:02 -0400

    Intro to Singleton Design pattern

--- core/src/main/java/hello/core/AppConfig.java
+++ core/src/main/java/hello/core/AppConfig.java
@@ -9,17 +9,32 @@ import hello.core.order.OrderServiceImpl;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-@Configuration // 애플리케이션의 설정 정보를 담는
+@Configuration // 앱의 config(설정, 구성) 스프링 설정 정보에는 항상 configuration 을 넣자
 public class AppConfig { // 애플리케이션의 실제 동작에 필요한 구현 객체를 생성
-    // 앱의 config(설정, 구성) 을 해주는 class (생성자 주입)
+
+    //@Bean memberService -> new MemoryMemberRepository()
+    //@Bean orderService -> new MemoryMemberRepository()
+
+    //"call AppConfig.memberService"
+    //"call AppConfig.memberRepository"
+    //"call AppConfig.orderService"
+    //"call AppConfig.memberRepository"
+    //"call AppConfig.memberRepository"
+
+    // 실제 호출된 메서드 멤버 리포지토리가 1번만 호출되었다
+//    call AppConfig.memberService
+//    call AppConfig.memberRepository
+//    call AppConfig.orderService
 
     @Bean
     public MemberService memberService(){
+        System.out.println("call AppConfig.memberService");
         return new MemberServiceImpl(memberRepository());
     }
 
     @Bean
     public OrderService orderService(){
+        System.out.println("call AppConfig.orderService");
         return new OrderServiceImpl(memberRepository(),discountPolicy());
     }
 
@@ -30,7 +45,8 @@ public class AppConfig { // 애플리케이션의 실제 동작에 필요한 구
     }
 
     @Bean
-    public MemberRepository memberRepository(){ // 역할을 더욱 명확하게 구분하여 코드의 가독성을 높임
+    public MemberRepository memberRepository(){
+        System.out.println("call AppConfig.memberRepository");// 역할을 더욱 명확하게 구분하여 코드의 가독성을 높임
         return new MemoryMemberRepository();
     }
 
---
+++ core/src/test/java/hello/core/singleton/ConfigurationSingleton.java
@@ -0,0 +1,37 @@
+package hello.core.singleton;
+
+import hello.core.AppConfig;
+import hello.core.member.MemberRepository;
+import hello.core.member.MemberService;
+import hello.core.member.MemberServiceImpl;
+import hello.core.order.OrderServiceImpl;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class ConfigurationSingleton {
+
+    @Test
+    void configurationTest(){
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberServiceImpl memberService = ac.getBean("memberService", MemberServiceImpl.class);
+        OrderServiceImpl orderService = ac.getBean("orderService", OrderServiceImpl.class);
+
+        MemberRepository memberRepository1 = memberService.getMemberRepository();
+        MemberRepository memberRepository2 = orderService.getMemberRepository();
+        System.out.println("orderService -> memberRepository2 = " + memberRepository2);
+        System.out.println("memberService ->memberRepository1 = " + memberRepository1);
+
+        Assertions.assertSame(memberRepository1,memberRepository2);
+    }
+
+    @Test
+    void configurationDeep(){
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+        AppConfig bean = ac.getBean(AppConfig.class);
+
+        System.out.println("bean = " + bean);
+
+    }
+}
